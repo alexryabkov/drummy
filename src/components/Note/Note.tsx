f812df596b1d1bfda1e09651a4640820ffdbe7a0
Alexander Ryabkov <alexryabkov@gmail.com>
2022-08-07 13:56:17 +0300

    Note: add "playing" and "select" styling

--- src/components/Note/Note.tsx
+++ src/components/Note/Note.tsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import "./styles/Note.scss";
 
-function Note() {
+type Props = {
+  playing: boolean;
+};
+
+function Note({ playing }: Props) {
   const [selected, setSelected] = useState(false);
-  return (
-    <button
-      className={selected ? "note note-selected" : "note"}
-      onClick={() => setSelected(!selected)}
-    />
-  );
+
+  function noteClass() {
+    return playing ? "note note-playing" : selected ? "note note-selected" : "note";
+  }
+
+  return <button className={noteClass()} onClick={() => setSelected(!selected)} />;
 }
 
 export default Note;
--- src/components/NotesPanel/NotesPanel.tsx
+++ src/components/NotesPanel/NotesPanel.tsx
@@ -1,15 +1,35 @@
 import Note from "components/Note/Note";
+import { useEffect, useState } from "react";
 import "./styles/NotesPanel.scss";
 
 function Notes() {
-  const numOfNotes = 16;
+  const notesInGroup = 16;
+  const numOfGroups = 12;
   const groups: JSX.Element[] = [];
+  const [playNoteId, setPlayNoteId] = useState(0);
+  const tempo = 120; //beats per minute
 
-  for (let i = 0; i < numOfNotes; i++) {
-    groups.push(<Note key={`note-${i + 1}`} />);
+  useEffect(() => {
+    function updatePlayNoteId() {
+      setPlayNoteId((playNoteId) => (playNoteId < notesInGroup - 1 ? playNoteId + 1 : 0));
+    }
+
+    updatePlayNoteId();
+    const interval = setInterval(updatePlayNoteId, (1000 * 60) / tempo);
+    return () => clearInterval(interval);
+  }, []);
+
+  for (let i = 0; i < numOfGroups; i++) {
+    const notesGroup: JSX.Element[] = [];
+    const groupClassName = i > 0 ? "notes notes-hidden" : "notes";
+    for (let j = 0; j < notesInGroup; j++) {
+      notesGroup.push(<Note key={`group-${i + 1}-note-${j + 1}`} playing={j === playNoteId} />);
+    }
+
+    groups.push(<div className={groupClassName}>{notesGroup}</div>);
   }
 
-  return <div className="notes">{groups}</div>;
+  return <>{groups}</>;
 }
 
 export default Notes;
